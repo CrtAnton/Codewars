5caf45aef3ba7a3288e86cbceb13eba2dfae6584
CrtAnton <108093422+CrtAnton@users.noreply.github.com>
2022-07-11 14:16:41 +0100

    Create L8 Sum of positive.cs

---
+++ C#/L8 Sum of positive.cs
@@ -0,0 +1,23 @@
+/*You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.
+*/
+
+using System;
+using System.Linq;
+
+public class Kata
+{
+  public static int PositiveSum(int[] arr)
+  {
+    int sum = 0;
+    foreach(int number in arr){
+      if (number > 0){
+        sum += number;
+      }
+    }
+    return sum;
+  }
+}
