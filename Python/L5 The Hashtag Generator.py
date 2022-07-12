b789f82c4d3f3ff1bfb02c063d58364e7d7f1b54
CrtAnton <108093422+CrtAnton@users.noreply.github.com>
2022-07-12 22:47:12 +0100

    Create L5 The Hashtag Generator.py

---
+++ Python/L5 The Hashtag Generator.py
@@ -0,0 +1,15 @@
+# Here's the deal:
+
+# It must start with a hashtag (#).
+# All words must have their first letter capitalized.
+# If the final result is longer than 140 chars it must return false.
+# If the input or the result is an empty string it must return false.
+# Examples
+# " Hello there thanks for trying my Kata"  =>  "#HelloThereThanksForTryingMyKata"
+# "    Hello     World   "                  =>  "#HelloWorld"
+# ""                                        =>  false
+
+def generate_hashtag(s):
+    new = "#"
+    new += "".join([x.capitalize() for x in s.split(" ")])
+    return False if (len(new)>140 or len(s) == 0) else new
