14c92c7b8477a5629e18477081080b755ebd4391
CrtAnton <108093422+CrtAnton@users.noreply.github.com>
2022-07-14 17:28:49 +0100

    Create L6 Duplicate Encoder.py

---
+++ Python/L6 Duplicate Encoder.py
@@ -0,0 +1,25 @@
+# The goal of this exercise is to convert a string to a new string where each 
+# character in the new string is "(" if that character appears only once in the 
+# original string, or ")" if that character appears more than once in the original string. 
+# Ignore capitalization when determining if a character is a duplicate.
+
+# Examples
+# "din"      =>  "((("
+# "recede"   =>  "()()()"
+# "Success"  =>  ")())())"
+# "(( @"     =>  "))((" 
+
+def duplicate_encode(word):
+    return "".join([")" if word.lower().count(letter) > 1 else "(" for letter in word.lower()])
+  
+# or 
+
+def duplicate_encode(word):
+    new = ""
+    for letter in word.lower():
+        if word.lower().count(letter) > 1:
+            new += ")"
+        else:
+            new += "("
+    return new
+    
