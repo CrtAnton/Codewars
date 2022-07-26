aca53b4afa79c672febf942eb47dbfa4f514a65a
CrtAnton <108093422+CrtAnton@users.noreply.github.com>
2022-07-26 17:16:43 +0100

    Create L6 Twisted Sum.py

---
+++ Python/L6 Twisted Sum.py
@@ -0,0 +1,18 @@
+# Find the sum of the digits of all the numbers from 1 to N (both ends included).
+
+# Examples
+# # N = 4
+# 1 + 2 + 3 + 4 = 10
+
+# # N = 10
+# 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + (1 + 0) = 46
+
+# # N = 12
+# 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + (1 + 0) + (1 + 1) + (1 + 2) = 51
+
+
+def compute_sum(n):
+    result = 0
+    for x in range(n+1):
+        result += sum([int(x) for x in str(x)])
+    return result
